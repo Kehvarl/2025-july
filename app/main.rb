a4f2d84eeee76b14c181e57cdde4463fa0ad22c1
Kehvarl <Kehvarl@Kehvarl.com>
2025-07-16 19:39:59 -0700

    playing with gravity

--- app/main.rb
+++ app/main.rb
@@ -1,2 +1,31 @@
+
+def dist(m1, m2)
+    dx = abs(m1.x - m2.x)
+    dy = abs(m1.y - m2.y)
+    return Math.sqrt(dx^2 + dy^2)
+end
+
+def calc_gravity (m1, m2)
+    G = 6.674e-11
+    d = dist(m1, m2)
+    return G * ((m1.m * m2.m)/d^2)
+end
+
+def init args
+    args.state.center = {x: 640, y: 360, anchor_x: 0.5, anchor_y: 0.5,
+                         w: 4, h: 4, path: "sprites/misc/tiny-star.png",
+                         m: 5.0e30}.sprite!
+    args.state.ship = {x: 400, y: 100, anchor_x: 0.5, anchor_y: 0.5,
+                       w: 16, h: 16, path: "sprites/misc/lowrez-ship-blue.png",
+                       m: 10}.sprite!
+end
+
 def tick args
+    if Kernel.tick_count <= 0
+        init args
+    end
+
+    args.outputs.primitives << args.state.center
+    args.outputs.primitives << args.state.ship
+
 end
