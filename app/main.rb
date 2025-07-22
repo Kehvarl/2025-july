eb7c97f8eb787e475da5c162be9964ff69588239
Kehvarl <Kehvarl@Kehvarl.com>
2025-07-21 19:43:39 -0700

    gravity stuff

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,8 @@
 
 def calc_gravity (m1, m2)
     G = 0.05
+    # Calculate the gravitational force between 2 points in a plane
+    # Return the X and Y vector components of the force.
     dx = m2.x - m1.x
     dy = m2.y - m1.y
     d2 = dx**2 + dy**2
@@ -13,17 +15,36 @@ def calc_gravity (m1, m2)
     return fx, fy
 end
 
+def deg2rad angle
+    return angle * (Math::PI / 180)
+end
+
 def thrust_vector ship
-    # Calculate Thrust in 2d space with the same properties as DragonRuby uses for Sprites
+    # Calculate Thrust in a plane
+    # Apply the X and Y vectors to the ship
+
+    # Assume space with the same properties as DragonRuby uses for Sprites
     # 0-degrees:  Positive X
     # Rotation :  Counterclockwise
 
     # Offset sprite rotation 90 degrees Counterclockwise to account for sprite orientation
-    a_rad = (ship.angle + 90)  * (Math::PI / 180)
+    a_rad = deg2dad(ship.angle + 90)
 
     # Calculate X and Y components of total Thrust vector.
     ship.vx += Math.cos(a_rad) * ship.thrust
     ship.vy += Math.sin(a_rad) * ship.thrust
+end
+
+def calc_circular_orbit(ship, star)
+    G = 0.05
+    dx = ship.x - star.x
+    dy = ship.y - star.y
+    r = Math.sqrt(dx**2 + dy**2)
+    v = Math.sqrt(G * (star.m / r))
+    tx = -dy/r
+    ty = -dx/r
+    ship.vx = tx * v
+    ship.vy = ty * v
 end
 
 def init args
@@ -47,6 +68,7 @@ end
 def tick args
     if Kernel.tick_count <= 0
         init args
+        calc_circular_orbit(args.state.player, args.state.center)
     end
 
     if args.inputs.keyboard.left
